cf24fcacf6d57cff12582335b3b00e5c38c1ea78
TarhunNyan <stepanov_grigorii99@mail>
2023-09-28 17:44:42 +0700

    Add if__... functions

--- jssupport.js
+++ jssupport.js
@@ -46,20 +46,80 @@ function if__eqPrim(value, value2) {
  * @return {boolean} возвращает, являются ли значения массивов одинаковыми
  */
 function if__eqArrOfPrim(value, value2) {
-    const l = value.length;
-    if (value.length === value2.length) {
-        for (let i = 0; i < l; i++) {
-            if (value[i] != value2[i]) { return false }
-        }
-        return true;
+    if (value == value2) { return true; }
+    if (value == null || value2 == null) { return false; }
+    if (value.length != value2.length) { return false; }
+
+    for (let i = 0; i < value.length; i++) {
+        if (value[i] != value2[i]) { return false }
     }
-    return false;
+
+    return true;
+}
+
+/**
+ * Условие, являются ли массивы(рекурсивно) эквивалентными
+ * @param {array} value Первый массив
+ * @param {array} value2 Второй массив
+ * @return {boolean} возвращает, являются ли значения массивов одинаковыми
+ */
+function if__eqArr(value, value2) {
+    if (value == value2) { return true; }
+    if (value == null && value2 == null) { return false; }
+    if (value.length != value2.length) { return false; }
+
+    for (let i = 0; i < value.length; i++) {
+        const el = value[i], el2 = value2[i];
+        const isArr_el = Array.isArray(el), isArr_el2 = Array.isArray(el2);
+
+        if (isArr_el != isArr_el2) { return false; }
+        if ((isArr_el == isArr_el2 && isArr_el == false) && el != el2) { return false; }
+        if ((isArr_el == isArr_el2 && isArr_el == true) && if__eqArr(el, el2) == false) { return false; }
+    }
+
+    return true;
+}
+
+/**
+ * Условие, являются ли объекты(рекурсивно) эквивалентными. Через JSON.stringify
+ * @param {array} value Первый объект
+ * @param {array} value2 Второй объект
+ * @return {boolean} возвращает, являются ли объекты одинаковыми
+ */
+function if__eqObjFast(value, value2) {
+    if (value == value2) { return true; }
+    if (value == null && value2 == null) { return false; }
+
+    return JSON.stringify(value) === JSON.stringify(value2);
+}
+
+/**
+ * Условие, являются ли объекты(рекурсивно) эквивалентными. Только для enumerable свойств
+ * @param {array} value Первый объект
+ * @param {array} value2 Второй объект
+ * @return {boolean} возвращает, являются ли объекты одинаковыми
+ */
+function if__eqObj(value, value2) {
+    if (value == value2) { return true; }
+    if (value == null && value2 == null) { return false; }
+
+    const objKeys = Object.keys(value), objKeys2 = Object.keys(value2);
+    if (objKeys.length != objKeys2.length) { return false; }
+    for (let i = 0; i < objKeys.length; i++) {
+        const prop = value[objKeys[i]], prop2 = value2[objKeys[i]];
+        let res = true;
+
+        if (prop != prop2) { res = false; }
+        if (typeof prop === 'object') { res = if__eqObj(prop, prop2); }
+        if (res == false) { return false; }
+    }
+    return true;
 }
 
 /**
  * Являются ли длины массивов одинаковыми. Так же можно сравнить массив с числом, выдаст равны ли они
- * @param {array|number} value Первый массив или число
- * @param {array|number} value2 Второй массив или число
+ * @param {array|number} value Первый массив/число
+ * @param {array|number} value2 Второй массив/число
  * @return {boolean} возвращает, являются ли одинаковыми длины массивов
  */
 function if__eqArrLength(value, value2) {
@@ -72,9 +132,9 @@ function if__eqArrLength(value, value2) {
 }
 
 /**
- * Являются ли длины массивов одинаковыми. Так же можно сравнить массив с числом, выдаст равны ли они
- * @param {array|numeric} value Первый массив или число
- * @param {array|numeric} value2 Второй массив или число
+ * Являются ли объекты коллекций одной длины. Так же можно сравнить с числом, выдаст равны ли они
+ * @param {array|numeric} value Первая коллекция/число
+ * @param {array|numeric} value2 Вторая коллекция/число
  * @return {boolean} возвращает, являются ли одинаковыми длины массивов
  */
 function if__eqLength(value, value2) {
@@ -83,6 +143,72 @@ function if__eqLength(value, value2) {
 
     if (l === l2) { return true; }
     return false;
+}
+
+/**
+ * Условие, являются ли строкой
+ * @param {array} value Проверяем значение
+ * @return {boolean} Являются ли строкой
+ */
+function if__isString(value) {
+    return typeof value === 'string';
+}
+
+/**
+ * Условие, являются ли не пустой строкой
+ * @param {array} value Проверяем значение
+ * @return {boolean} Являются ли не пустой строкой
+ */
+function if__isStringContent(value) {
+    return typeof value === 'string' && value != '';
+}
+
+/**
+ * Является ли символ числом
+ * @param {String} str Строка из которой берем символ
+ * @param {Number} [index] Индекс символа в строке, который проверяем. По умолчанию 0
+ * @return {boolean} Является ли символ числовым
+ */
+function if__isStringNumeric(str, index = 0) {
+    const s = str.charCodeAt(index);
+    // return s >= "0".charCodeAt(0) && s <= "9".charCodeAt(0);
+    return s >= 48 && s <= 57;
+}
+
+/**
+ * Получить числовую последовательность символов
+ * @param {String} str Строка из которой берем символ
+ * @param {Number} [index] Индекс начиная с которого смотрим. По умолчанию 0
+ * @return {String} Подстрока, в которой число
+ */
+function string__getNumeric(str, index = 0) {
+    let i = index;
+    for (let s = str[i]; i < str.length && if__isStringNumeric(s); i++, s = str[i]) { }
+    return str.slice(index, i);
+}
+
+/**
+ * Получить длину числовой последовательности символов
+ * @param {String} str Строка из которой берем символ
+ * @param {Number} [index] Индекс начиная с которого смотрим. По умолчанию 0
+ * @return {Number} Длина числовой последовательности
+ */
+function string__getNumericLength(str, index = 0) {
+    let i = index;
+    for (let s = str[i]; i < str.length && if__isStringNumeric(s); i++, s = str[i]) { }
+    return i - index;
+}
+
+/**
+ * Получить конец числовой последовательности символов
+ * @param {String} str Строка из которой берем символ
+ * @param {Number} [index] Индекс начиная с которого смотрим. По умолчанию 0
+ * @return {Number} Индекс следущего символа после числовой последовательности
+ */
+function string__getNumericEnd(str, index = 0) {
+    let i = index;
+    for (let s = str[i]; i < str.length && if__isStringNumeric(s); i++, s = str[i]) { }
+    return i;
 }
 
 /**
@@ -125,7 +251,7 @@ function convert__stringToBoolean(value) {
  */
 
 /**
- * По функционалу это копипаста с Array.map, но ничего не возвращает. Проходит функцией по массиву
+ * По функционалу это копипаста с Array.forEach, но ничего не возвращает. Проходит функцией по массиву
  * @param {[]} arr Массив по которому проходимся
  * @param {forCb} func Функция принимающая индекс, текущий элемент, весь массив
  */
@@ -235,8 +361,19 @@ module.exports = {
     if__false,
     if__eqPrim,
     if__eqArrOfPrim,
+    if__eqArr,
+    if__eqObjFast,
+    if__eqObj,
     if__eqArrLength,
     if__eqLength,
+
+    if__isString,
+    if__isStringContent,
+    if__isStringNumeric,
+
+    string__getNumeric,
+    string__getNumericLength,
+    string__getNumericEnd,
 
     itter__length,
 
@@ -248,4 +385,5 @@ module.exports = {
     for2__CashArr,
 
     func__compose,
+    func__curry,
 };
